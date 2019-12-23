6dfdc4e3be5ca8896626ec3661b1a8ea4ca10699
dmitriy.sinichkin <dmitriy.sinichkin@gmail.com>
2019-12-23 12:12:52 +0300

    PAY-0 unused env vars remove

--- internal/dispatcher/common/config.go
+++ internal/dispatcher/common/config.go
@@ -4,7 +4,6 @@ type Config struct {
 	CookieDomain string `envconfig:"COOKIE_DOMAIN" required:"true"`
 	AllowOrigin  string `envconfig:"ALLOW_ORIGIN" default:"*"`
 
-	PaymentFormJsLibraryUrl string `envconfig:"PAYMENT_FORM_JS_LIBRARY_URL" required:"true"`
 	// OrderInlineFormUrlMask url like a https://checkout.tst.pay.super.com/pay/order/
 	OrderInlineFormUrlMask string `envconfig:"ORDER_INLINE_FORM_URL_MASK" required:"true"`
 
